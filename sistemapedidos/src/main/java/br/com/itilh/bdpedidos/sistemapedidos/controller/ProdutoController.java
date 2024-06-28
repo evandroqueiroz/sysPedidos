07764d98ee5c2ef4813f59d3cb7a43c601efd127
Evandro <evandroqz@gmail.com>
2024-06-28 20:09:53 -0300

    Alterações no Projeto

--- sistemapedidos/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/controller/ProdutoController.java
+++ sistemapedidos/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/controller/ProdutoController.java
@@ -1,59 +1,66 @@
 package br.com.itilh.bdpedidos.sistemapedidos.controller;
 
-import org.springframework.web.bind.annotation.RestController;
 import br.com.itilh.bdpedidos.sistemapedidos.model.Produto;
 import br.com.itilh.bdpedidos.sistemapedidos.repository.ProdutoRepository;
+
 import java.math.BigInteger;
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 public class ProdutoController {
-   private final ProdutoRepository repositorio;
-        public ProdutoController (ProdutoRepository repositorio){
-            this.repositorio = repositorio;
-        }
-@GetMapping("/produto")
-public List<Produto> getProduto() {
-    return (List<Produto>) repositorio.findAll();
-}
- @GetMapping("/produto/{id}")
-    public Produto getPorId(@PathVariable BigInteger id) throws Exception {
-        return repositorio.findById(id).orElseThrow(
-            () -> new Exception("ID inválido."));
-    }    
-@PostMapping("/produto")
-    public Produto criarProduto(@RequestBody Produto entity) throws Exception { 
-        try{               
+    private final ProdutoRepository repositorio;
+
+    public ProdutoController(ProdutoRepository repositorio) {
+        this.repositorio = repositorio;
+    }
+
+    @GetMapping("/produtos")
+    public List<Produto> getProdutoTodos() {
+        return (List<Produto>) repositorio.findAll();
+    }
+
+    @GetMapping("/produto/{id}")
+    public Produto getProdutosPorId(@PathVariable BigInteger id) throws Exception {
+        return repositorio.findById(id).orElseThrow(()-> new Exception("Id não encontrado"));
+    }
+
+    @PostMapping("/produto")
+    public Produto AdicinarProduto(@RequestBody Produto entity) throws Exception {
+        try {
             return repositorio.save(entity);
-        }catch(Exception e){
-            throw new Exception("Erro ao salvar o produto.");
+        }catch (Exception ex) {
+            throw new Exception("Não foi possível adicionar o produto no banco de dados." + ex.getMessage());
         }
     }
-@PutMapping("/produto/{id}")
-    public Produto alterarEstado(@PathVariable BigInteger id, 
-        @RequestBody Produto novosDados) throws Exception {
-        Optional<Produto> produtoAmazenado = repositorio.findById(id);
-        if(produtoAmazenado.isPresent()){
-            produtoAmazenado.get().setNome(novosDados.getNome());
-            return repositorio.save(produtoAmazenado.get());
-        }        
-        throw new Exception("Alteração não foi realizada.");
+
+    @PutMapping("produto/{id}")
+    public Produto AlterarProduto(@PathVariable BigInteger id, @RequestBody Produto novosDados) throws Exception {
+        Optional<Produto> produtoArmazenado = repositorio.findById(id);
+        if(produtoArmazenado.isPresent()) {
+            //Atribuir novo nome aos objetos que já existem no banco de dados
+            produtoArmazenado.get().setDescricao(novosDados.getDescricao());
+
+            return repositorio.save(produtoArmazenado.get());
+        }
+        throw new Exception("Alteração não realizada");
     }
-@DeleteMapping("/produto/{id}")
-    public String deletePorId(@PathVariable BigInteger id) throws Exception {
-        Optional<Produto> produtoAmazenado = repositorio.findById(id);
-        if(produtoAmazenado.isPresent()){
-            repositorio.delete(produtoAmazenado.get());
+
+    @DeleteMapping("produto/{id}")
+    public String deleteProdutoPorId(@PathVariable BigInteger id) throws Exception {
+        Optional<Produto> produtoArmazenado = repositorio.findById(id);
+        if(produtoArmazenado.isPresent()) {
+            repositorio.delete(produtoArmazenado.get());
             return "Excluído";
         }
-        throw new Exception("Id não econtrado para a exclusão");
-    }    
+        throw new Exception("Id não localizado");
+    }
 }
--- sistemapedidos/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/model/Produto.java
+++ sistemapedidos/src/main/java/br/com/itilh/bdpedidos/sistemapedidos/model/Produto.java
@@ -1,5 +1,6 @@
 package br.com.itilh.bdpedidos.sistemapedidos.model;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 
 import jakarta.persistence.Column;
@@ -16,46 +17,31 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
-@EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 @ToString
-@Entity
-@Table(name ="tb_produtos")
 
+@Entity
+@Table(name = "tb_produtos")
 public class Produto {
 
     @Id
-    @SequenceGenerator(name = "seq_produto_id", sequenceName = "tb_produtos_id_seq", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_produto_id")
+    @SequenceGenerator(name = "produtos_id_seq", 
+        sequenceName = "tb_produtos_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, 
+        generator = "produtos_id_seq")
     private BigInteger id;
-
 
     @Column(name = "tx_descricao")
     private String descricao;
 
-    
     @Column(name = "dbl_quantidade_estoque")
-    private Double quantidade_estoque;
+    private Double quantidadeEstoque;
 
-    @Column(name = "nu_preco_unidade")
-    private Number preco_unidade;
-
+    @Column(name = "nu_preco_unidade_atual")
+    private BigDecimal precoUnidadeAtual;
 
     @Column(name = "bo_ativo")
     private Boolean ativo;
-
-
-    public Object getNome() {
-        
-        throw new UnsupportedOperationException("Unimplemented method 'getNome'");
-    }
-
-
-    public void setNome(Object nome) {
-        
-        throw new UnsupportedOperationException("Unimplemented method 'setNome'");
-    }
-
-
 }
